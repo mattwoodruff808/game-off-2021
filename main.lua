53d0da5a3acaa3d3637d12021444f04105c240fd
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-11-09 18:25:42 -0700

    Initial Commit

---
+++ main.lua
@@ -0,0 +1,21 @@
+--[[
+    LOAD FUNCTION
+--]]
+function love.load()
+
+end
+
+--[[
+    UPDATE FUNCTION
+--]]
+function love.update(dt)
+
+end
+
+
+--[[
+    DRAW FUNCTION
+--]]
+function love.draw()
+
+end
